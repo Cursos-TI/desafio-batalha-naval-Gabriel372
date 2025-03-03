f4bdc139df5b74fa7ab9d37c816a6a3974c7217d
Gabriel Brandão <124525153+Gabriel372@users.noreply.github.com>
2025-03-03 21:40:51 +0000

    melhoria no codigo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -11,6 +11,40 @@ struct Coordenadas {
 void imprimirTabuleiro(int matriz[linha][coluna]);
 void perguntaInicial(int *x, int *y,char modoDePosicionamento[10],struct Coordenadas *coordenadas);
 void definirCoordenada(int *x, int *y, struct Coordenadas *coordenadas, int ehHorizontal);
+int stringEnumero(char string[100]);
+
+void criarTabuleiro(int matriz[linha][coluna]) {
+    for (int i = 0; i < linha ; i++) {
+for (int j = 0; j < coluna ; j++)
+{
+    matriz[i][j] = 0;
+}
+    }
+}
+
+void imprimirTabuleiro(int matriz[linha][coluna]) {
+ printf("           1a col.|2a col.|3a col.|4a col.|5a col.|6a col.\n");
+    for (int i = 0; i < linha ; i++) {
+            printf("%da linha   ",i + 1);
+for ( int j = 0; j < coluna ; j++)
+{
+    printf("   %d    ",matriz[i][j]);
+}
+    printf("\n");
+    }
+}
+
+void perguntaInicial(int *x,int *y,char modoDePosicionamento[10], struct Coordenadas *coordenadas){
+if (strcmp(modoDePosicionamento, "horizontal") == 0)
+{
+    int ehHorizontal = 1;
+    definirCoordenada(x,y,coordenadas,ehHorizontal);
+}
+else if (strcmp(modoDePosicionamento, "vertical") == 0){
+    int ehHorizontal = 0;
+    definirCoordenada(x,y,coordenadas,ehHorizontal);
+}
+}
 
 void posicionamentoNoTabuleiro(int matriz[6][6],int *x,int *y,char modoDePosicionamento[10]){
     if (strcmp(modoDePosicionamento, "horizontal") == 0){
@@ -28,50 +62,23 @@ else if (strcmp(modoDePosicionamento, "vertical") == 0){
 }
 }
 
-void criarTabuleiro(int matriz[linha][coluna]) {
-    for (int i = 0; i < linha ; i++) {
-for (int j = 0; j < coluna ; j++)
-{
-    matriz[i][j] = 0;
-}
-    }
-
-}
-
-void imprimirTabuleiro(int matriz[linha][coluna]) {
- printf("           1a col.|2a col.|3a col.|4a col.|5a col.|6a col.\n");
-    for (int i = 0; i < linha ; i++) {
-            printf("%da linha   ",i + 1);
+void resposta(int matriz[6][6],struct Coordenadas *coordenadas){
+        printf("======================================================================================\n");
+        printf("Resultado:\n");
+        printf("Navio posicionado na horizontal, coordenadas: (%da linha) e (%da coluna)\n",coordenadas->horizontal[0],coordenadas->horizontal[1]);
+        printf("Navio posicionado na vertical, coordenadas: (%da linha) e (%da coluna)\n", coordenadas->vertical[0],coordenadas->vertical[1]);
+        printf("\n");
+        for (int i = 0; i < linha; i++)
+        {
 for ( int j = 0; j < coluna ; j++)
 {
-    printf("   %d    ",matriz[i][j]);
+    printf("%d ",matriz[i][j]);
 }
     printf("\n");
     }
+    printf("\n");
 }
 
-int stringEnumero(char string[100]) { 
-    if (string[0] == '\0') return 0;
-    for (int i = 0; string[i] != '\0'; i++) {
-        if (string[i] < '0' || string[i] > '9') {
-            return 0;
-        }
-                      return 1;
-    }
-              return 1;
-}
-
-void perguntaInicial(int *x,int *y,char modoDePosicionamento[10], struct Coordenadas *coordenadas){
-if (strcmp(modoDePosicionamento, "horizontal") == 0)
-{
-    int ehHorizontal = 1;
-    definirCoordenada(x,y,coordenadas,ehHorizontal);
-}
-else if (strcmp(modoDePosicionamento, "vertical") == 0){
-        int ehHorizontal = 0;
-    definirCoordenada(x,y,coordenadas,ehHorizontal);
-}
-}
 void definirCoordenada(int *x, int *y, struct Coordenadas *coordenadas, int ehHorizontal) {
     char string[100];
     do {
@@ -84,7 +91,7 @@ void definirCoordenada(int *x, int *y, struct Coordenadas *coordenadas, int ehHo
         }
         *x = atoi(string) - 1;
         if (*x < 0 || *x > (ehHorizontal ? 5 : 3)) {
-            printf("Erro: posicao invalida.\n");
+        printf("Erro: posicao invalida.\n");
         }
     } while (!stringEnumero(string) || *x < 0 || *x > (ehHorizontal ? 5 : 3));
     do {
@@ -101,29 +108,23 @@ void definirCoordenada(int *x, int *y, struct Coordenadas *coordenadas, int ehHo
     } while (!stringEnumero(string) || *y < 0 || *y > (ehHorizontal ? 3 : 5));
 if (ehHorizontal) {
     coordenadas->horizontal[0] = *x + 1;
-        coordenadas->horizontal[1] = *y + 1;
+    coordenadas->horizontal[1] = *y + 1;
 }
 else{
-        coordenadas->vertical[0] = *x + 1;
-        coordenadas->vertical[1] = *y + 1;
+    coordenadas->vertical[0] = *x + 1;
+    coordenadas->vertical[1] = *y + 1;
 }
 }
 
-void resposta(int matriz[6][6],struct Coordenadas *coordenadas){
-            printf("======================================================================================\n");
-        printf("Resultado:\n");
-        printf("Navio posicionado na horizontal, coordenadas: (%da linha) e (%da coluna)\n",coordenadas->horizontal[0],coordenadas->horizontal[1]);
-        printf("Navio posicionado na vertical, coordenadas: (%da linha) e (%da coluna)\n", coordenadas->vertical[0],coordenadas->vertical[1]);
-        printf("\n");
-        for (int i = 0; i < linha; i++)
-        {
-for ( int j = 0; j < coluna ; j++)
-{
-    printf("%d ",matriz[i][j]);
-}
-    printf("\n");
+int stringEnumero(char string[100]) { 
+    if (string[0] == '\0') return 0;
+    for (int i = 0; string[i] != '\0'; i++) {
+        if (string[i] < '0' || string[i] > '9') {
+            return 0;
+        }
+     return 1;
     }
-    printf("\n");
+    return 1;
 }
 
 int main() {
